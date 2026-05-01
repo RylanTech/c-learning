7016a9b9410fb18c6b58b9662856398df160975f
Rylan Workman <rylanworkman9@gmail.com>
2026-05-01 15:22:58 -0400

    TLDR - Added modularity with a function. Made exercise3-3.c more modular by making the function 'expandSet' that will calculate the needed in-between characters. Changed the function 'expand' to call 'expandSet'

--- chapter3/3-5-While-and-For/exercise3-3.c
+++ chapter3/3-5-While-and-For/exercise3-3.c
@@ -2,6 +2,7 @@
 
 // Expand abbreviations from a-d to abcd, or 1-4 to 1234
 void expand(char s1[], char s2[]);
+void expandSet(char s1[], char s2[], int *s1Pos, int *s2Posi, char startingChar);
 int getString(char inputString[]);
 
 #define	MAXARRAYSIZE	100
@@ -34,51 +35,18 @@ void expand(char s1[], char s2[]) {
 			// If 'a' is greater than 0, but less than or equal to 9, and 'b' is greater than 'a' but less than or equal to 9
 			// This accounts for all integers
 			if ((s1[i-1] >= '0' && s1[i-1] <= '9') && (s1[i+1] >= s1[i-1]) && (s1[i+1] <= '9')) { // For all integers
-				// 1 is subtracted from itterationsNeeded because we will just use the ending number that the user already types in s1.
-				int itterationsNeeded = s1[i+1] - s1[i-1] - 1;
-				
-				// Creating a starting number (integer) from the character that is used to start with. e.g. character '1' translates to integer 1
-				int startingNumber = s1[i-1] - '0';
-
-				// Itterating through the difference between the starting integer and ending integer
-				for (int j = 1; j <= itterationsNeeded; j++) {
-					// Add the starting integer plus the current itteration starting from 1 (0 is added from s1 directly), then add '0' to turn it into its corresponding character type
-					s2[k++] = startingNumber + j + '0';
-				}
-
+				// Calculates the needed output based off of the starting position	
+				expandSet(s1, s2, &i, &k, '0');
 				i++;
 				continue;
-				
 			}
 			else if ((s1[i-1] >= 'a' && s1[i-1] <= 'z') && (s1[i+1] >= s1[i-1]) && (s1[i+1] <= 'z')) { // For all lowercase letters
-				// Gets the difference between the starting character and ending character
-				int itterationsNeeded = s1[i+1] - s1[i-1] - 1;
-
-				// Creates a starting number by subtracting the starting character 'a' to starting from the integer 0; 
-				int startingNumber = s1[i-1] - 'a';
-				
-				// Itterate through the difference between the starting character and ending character
-				for (int j = 1; j <= itterationsNeeded; j++) {
-					// Add the starting character plus the current itteration from 1 (0 is added from s1 directly), then add 'a' to turn it to its corresponding character type
-					s2[k++] = startingNumber + j + 'a';	
-				}
-				
+				expandSet(s1, s2, &i, &k, 'a');
 				i++;
 				continue;
 			}	
 			else if ((s1[i-1] >= 'A' && s1[i-1] <= 'Z') && (s1[i+1] >= s1[i-1]) && (s1[i+1] <= 'Z')) { // For all uppercase letters
-				// Gets the difference between the starting character and ending character 
-				int itterationsNeeded = s1[i+1] - s1[i-1] - 1;
-
-				// Creates a starting number by subtracting the starting character 'A' to starting from the integer 0; 
-				int startingNumber = s1[i-1] - 'A';
-				
-				// Itterate through the difference between the starting character and ending character
-				for (int j = 1; j <= itterationsNeeded; j++) {
-					// Add the starting character plus the current itteration from 1 (0 is added from s1 directly), then add 'A' to turn it to its corresponding character type
-					s2[k++] = startingNumber + j + 'A';	
-				}
-				
+				expandSet(s1, s2, &i, &k, 'A');
 				i++;
 				continue;
 			}
@@ -90,6 +58,20 @@ void expand(char s1[], char s2[]) {
 	
 	// Add a string termination character to the output string
 	s2[k] = '\0';
+}
+
+void expandSet(char s1[], char s2[], int *s1Pos, int *s2Pos, char startingChar) {
+	// Gets the difference between the starting character and ending character 
+	int itterationsNeeded = s1[(*s1Pos)+1] - s1[(*s1Pos)-1] - 1;
+
+	// Creates a starting number by subtracting the starting character to starting from the integer 0; 
+	int startingNumber = s1[(*s1Pos)-1] - startingChar;
+
+	// Itterate through the difference between the starting character and ending character
+	for (int j = 1; j <= itterationsNeeded; j++) {
+		// Add the starting character plus the current itteration from 1 (0 is added from s1 directly), then add the starting point to turn it to its corresponding character type	
+		s2[(*s2Pos)++] = startingNumber + j + startingChar;	
+	}
 }
 
 int getString(char inputString[]) {
